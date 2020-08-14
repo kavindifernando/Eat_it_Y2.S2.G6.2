757177f21da0cf3f6f9eda9d4854c0fd53d7e23a
kavindi <kavindifernando999@gmail.com>
2020-08-15 02:37:49 +0530

    connection added

--- app/src/main/java/com/example/eai_it/MainActivity.java
+++ app/src/main/java/com/example/eai_it/MainActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 
 import com.example.eai_it.ui.login.LoginActivity;
 
-import java.time.Instant;
+
 
 public class MainActivity extends AppCompatActivity {
 Button login1;
@@ -16,12 +17,13 @@ Button login1;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login1=findViewById(R.id.login1);
+        login1= findViewById(R.id.login1);
         login1.setOnClickListener((new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Instant instent =new  Instent(MainActivity.this, LoginActivity.class);
-                startActivity(instent);
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         }));
     }
